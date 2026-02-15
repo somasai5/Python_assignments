d2823c50d699621db989f5f31ce0f0c1881750ee
somasai05 <ksomasai6@gmail.com>
2026-02-15 18:56:14 +0530

    code after adding personalization and countings

--- challenge_Day3.py
+++ challenge_Day3.py
@@ -13,19 +13,25 @@ medium_risk = []
 high_risk = []
 critical_risk = []
 
-
+totalValid=0
+totalIgnored=0
+personalization=0
 
 for scores in activityScores:
     if scores>100:
         critical_risk += [scores]
+        totalValid = totalValid + 1
     elif scores>60:
         high_risk += [scores]
+        totalValid = totalValid + 1
     elif scores>30:
         medium_risk += [scores]
+        totalValid = totalValid + 1
     elif scores>0:
         low_risk += [scores]
+        totalValid = totalValid + 1
     else:
-
+        totalIgnored += 1
 
 print("\nlow risk :",low_risk)
 print("medium risk :",medium_risk)
@@ -45,3 +51,9 @@ print("low risk :",low_risk)
 print("medium risk :",medium_risk)
 print("high risk :",high_risk)
 print("critical risk :",critical_risk)
+
+print("\nTotal valid :",totalValid)
+print("Total ignored :",totalIgnored)
+print("Removed due to personalization :",personalization)
+
+
