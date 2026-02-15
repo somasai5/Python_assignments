196919d4a02054f582cbbaef3a8e64b7953a3b9f
somasai05 <ksomasai6@gmail.com>
2026-02-15 18:51:15 +0530

    Add files via upload

---
+++ day4_challenge.py
@@ -0,0 +1,40 @@
+registrationNumber = int(input("Enter your registration number: "))
+activityScores = []
+n = int(input("Enter number of activities: "))
+print("Enter activities: ")
+for i in range(n):
+    x = int(input())
+    activityScores += [x]
+print('activity Scores :',activityScores)
+
+low_risk = []
+medium_risk = []
+high_risk = []
+critical_risk = []
+
+totalValid=0
+totalIgnored=0
+
+
+for scores in activityScores:
+    if scores>100:
+        critical_risk += [scores]
+        totalValid = totalValid + 1
+    elif scores>60:
+        high_risk += [scores]
+        totalValid = totalValid + 1
+    elif scores>30:
+        medium_risk += [scores]
+        totalValid = totalValid + 1
+    elif scores>0:
+        low_risk += [scores]
+        totalValid = totalValid + 1
+    else:
+        totalIgnored += 1
+
+print("\nlow risk :",low_risk)
+print("medium risk :",medium_risk)
+print("high risk :",high_risk)
+print("critical risk :",critical_risk)
+
+
